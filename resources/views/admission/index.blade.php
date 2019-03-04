8514432aeae61e8b55561a4e84db731a36cf0c46
Riccardo Silvi <silvicardo@gmail.com>
2019-03-04 16:52:24 +0100

    admission index layout

--- resources/views/admission/index.blade.php
+++ resources/views/admission/index.blade.php
@@ -4,6 +4,56 @@
 
 @section('content')
 
-    Test Iscriviti
+    <section class="one_step_away container text-center py-5">
+      <h1>Sei ad un passo dalla tua nuova professione</h1>
+      <p class="w-50 m-auto px-5">Per assicurare la buona riuscita del corso, ogni candidato deve sostenere
+      un breve percorso di selezione. Le due caratteristiche fondamentali per
+      essere un perfetto studente Boolean sono la capacità di ragionamento
+      critico e la motivazione</p>
+    </section>
+
+    <section class="start_here container_fluid bg_boolean_blue py-5">
+      <div class="container">
+        <div class="row">
+          <div class="how_to col-md-6">
+            <div class="claim py-5">
+              <h3>La tua avventura inizia qui</h3>
+              <p>
+                Non è facile prendere la decisione di iniziare il corso Boolean.
+                 Lascia la tua email o il tuo numero di telefono per essere
+                 ricontattato. Se preferisci chiamaci o lasciaci un messaggio
+                 al 02-40031288
+              </p>
+            </div>
+            <ul class="steps list-unstyled">
+              <li>Contattaci</li>
+              <li>Visita al campus virtuale</li>
+              <li>Test d'ingresso</li>
+              <li>Iscrizione</li>
+            </ul>
+          </div>
+          <div class="request_form col-md-6">
+            <form action="" class="py-5">
+              <div class="form-row">
+               <div class="form-group col-md-6">
+                 <input type="text" class="form-control" placeholder="Nome*">
+               </div>
+               <div class="form-group col-md-6">
+                 <input type="email" class="form-control" placeholder="Email*">
+               </div>
+             </div>
+             <div class="form-group">
+                <textarea class="form-control" rows="6" placeholder="Messaggio..."></textarea>
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="privacyCheck">
+                <label class="form-check-label text-white" for="privacyCheck">Accetto la Privacy Policy</label>
+              </div>
+              <button type="submit" class="btn_boolean submit btn_orange">Invia</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
 
 @endsection
